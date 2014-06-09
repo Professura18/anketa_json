c0c071ea26a8cff60b642ac98b341b2a9dce1d4d
Professura18 <Professura18@mail.ru>
2014-06-09 23:00:11 +0400

    Update studenst.js

--- studenst.js
+++ studenst.js
@@ -1,5 +1,26 @@
-function StudentCtrl($scope, $http) {
+var phonecatApp = angular.module('studentApp', []);
+
+var StudentCtrl = function ($scope, $http,  $locale) {
 	$http.get('students.json').success(function(data) {
     	$scope.students = data;
   	}); 
+	$scope.predicate = '';
+
+	$scope.remaining = function() {
+		return $index;
+	};
+	
+	$scope.openAnket = function(anket) {
+		alert(anket.fio.firstName);
+	};
+	
+	$scope.recordWordForms = {
+			0: '{} записей',
+			one: '{} запись',
+			few: '{} записи',
+			many: '{} записи',
+			other: '{} записей'
+ 	};
+
 }
+StudentCtrl.$inject = ['$scope', '$http', '$locale'];
